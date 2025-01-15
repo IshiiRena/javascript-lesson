28eed38ee67564c35d5d43bba97a5726ac10d7e2
IshiiRena <ishii.rena1127@gmail.com>
2025-01-15 16:49:30 +0900

    first commit

---
+++ practice/app.js
@@ -0,0 +1,43 @@
+let text = 'JavaScriptの練習';
+console.log(text);
+
+text = 'JavaScriptをマスターした';
+console.log(text);
+
+let longText =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+console.log(longText);
+console.log(longText);
+console.log(longText);
+
+// 処理②
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+console.log(
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+);
+
+let name = 'ジョニー';
+let greet = '私は' + name + '！';
+console.log(greet);
+
+let template = 'テンプレートリテラル'; // ※ これは文字列
+let templateText = `これが${template}です。
+${template}は改行もできます。`; // これはテンプレートリテラル
+console.log(templateText);
+
+let num = 10;
+console.log(num);
+let numTen = 10;
+let strTen = '10'; // 追記
+console.log(numTen, strTen); // 値を追加
+
+let single = '文字列型です';
+console.log(single);
+
+let double = '英語だとstringといいます。';
+console.log(double);
